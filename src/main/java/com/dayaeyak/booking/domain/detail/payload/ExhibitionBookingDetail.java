e0eb5a15d437ebe41610719d2636225f831f1887
Stock05 <qace756@gmail.com>
2025-09-15 04:07:12 +0900

    refactor: class -> record 변경, 의존성 추가

--- src/main/java/com/dayaeyak/booking/domain/detail/payload/ExhibitionBookingDetail.java
+++ src/main/java/com/dayaeyak/booking/domain/detail/payload/ExhibitionBookingDetail.java
@@ -1,16 +1,19 @@
 package com.dayaeyak.booking.domain.detail.payload;
 
-import com.dayaeyak.booking.domain.detail.enums.Grade;
+import com.dayaeyak.booking.domain.booking.enums.ServiceType;
+import com.dayaeyak.booking.enums.Grade;
+import com.dayaeyak.booking.enums.Grade;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ExhibitionBookingDetail implements BookingDetailPayload {
-    private Grade grade;
-    private int price;
+
+public record ExhibitionBookingDetail(
+        ServiceType type,
+        Grade grade,
+        int price
+
+) implements BookingDetailPayload {
+
 }
--- src/main/java/com/dayaeyak/booking/domain/detail/payload/PerformanceBookingDetail.java
+++ src/main/java/com/dayaeyak/booking/domain/detail/payload/PerformanceBookingDetail.java
@@ -1,19 +1,22 @@
 package com.dayaeyak.booking.domain.detail.payload;
 
+import com.dayaeyak.booking.domain.booking.enums.ServiceType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class PerformanceBookingDetail implements BookingDetailPayload {
-    private String sectionInfo;
-    private int seatNumber;
-    private LocalDate session;
-    private int seatPrice;
+
+public record PerformanceBookingDetail(
+        ServiceType type,
+        String sectionInfo,
+        int seatNumber,
+        LocalDateTime session,
+        int seatPrice
+
+) implements BookingDetailPayload {
+
 }
