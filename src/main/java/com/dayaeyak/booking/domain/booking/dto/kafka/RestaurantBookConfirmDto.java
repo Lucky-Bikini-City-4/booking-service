00e7667bf55c3c4a1caf49b1ed3439b43c9036f6
Stock05 <qace756@gmail.com>
2025-09-15 04:09:05 +0900

    refactor: import 누락

--- src/main/java/com/dayaeyak/booking/domain/booking/dto/kafka/RestaurantBookConfirmDto.java
+++ src/main/java/com/dayaeyak/booking/domain/booking/dto/kafka/RestaurantBookConfirmDto.java
@@ -1,5 +1,9 @@
 package com.dayaeyak.booking.domain.booking.dto.kafka;
 
+import com.dayaeyak.booking.domain.booking.enums.ServiceType;
+
+import java.time.LocalDateTime;
+
 public record RestaurantBookConfirmDto(
         Long userId,
         ServiceType serviceType,
